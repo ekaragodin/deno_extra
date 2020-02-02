5557a050ab8e47f8f0e6d902b45d994493645a4e
Evgenii Karagodin <ekaragodin@gmail.com>
2020-02-02 17:12:24 +0700

    Bump std version

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-import * as testing from "https://deno.land/std@v0.11.0/testing/mod.ts";
-import * as asserts from "https://deno.land/std@v0.11.0/testing/asserts.ts";
+import * as testing from "https://deno.land/std@v0.31.0/testing/mod.ts";
+import * as asserts from "https://deno.land/std@v0.31.0/testing/asserts.ts";
 
-export { testing, asserts }
+export { testing, asserts };
